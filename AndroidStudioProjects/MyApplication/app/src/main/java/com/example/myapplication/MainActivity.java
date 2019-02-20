67aa19374895b8eb0d1c658adb9d8a06a9c9ee8c
hhuang36 <hhuang36@buffalo.edu>
2019-02-20 18:47:48 -0500

    Implemented button to enter new activity

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Handler;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout menuDrawerLayout;
     private ActionBarDrawerToggle menuToggle;
+    //private ImageButton imageButton;
+    private Button floorplanButton;
     private static int SPLASH_TIME_OUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +39,20 @@ public class MainActivity extends AppCompatActivity {
         menuDrawerLayout.addDrawerListener(menuToggle);
         menuToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //imageButton = findViewById(R.id.imageButton);
+        floorplanButton = findViewById(R.id.floorplanButton);
+        floorplanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFloorPlan();
+            }
+        });
     }
 
+    public void openFloorPlan(){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem x){
         if(menuToggle.onOptionsItemSelected(x)) {
