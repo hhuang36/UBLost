5a4409b9e74bbc33643a7ca6e14d851f0b3351fa
hhuang36 <hhuang36@buffalo.edu>
2019-03-28 11:42:41 -0400

    I started creating JUnit tests for the login page. I created the LoginTest.java test file which contains 9 different tests. I started testing that the UI displays all the proper elements when the application is run. For example, I made sure the appropriate items such as textViews, buttons, etc. are displayed. We still need to add more tests to thoroughly check that our login screen is functional. We will do this in the following weeks. Additionally, I also modified Login.java and activity_login.xml in order to create these tests.

---
+++ AndroidStudioProjects/MyApplication/app/src/androidTest/java/com/example/myapplication/LoginTest.java
@@ -0,0 +1,89 @@
+package com.example.myapplication;
+
+import android.support.test.rule.ActivityTestRule;
+import android.text.Layout;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginTest extends android.app.Activity{
+
+    @Rule
+    public ActivityTestRule<LoginTest> loginTestActivityTestRule = new ActivityTestRule(LoginTest.class);
+    public LoginTest lT = null;
+
+    @Before
+    public void setUp() throws Exception {
+        lT = loginTestActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testLaunch(){
+        assertNotNull(lT);
+    }
+
+    @Test
+    public void testImageViewExists(){
+        ImageView iv = lT.findViewById(R.id.UBLostLogo);
+        assertNotNull(iv);
+    }
+
+    @Test
+    public void testScrollViewExists(){
+        ScrollView sv = lT.findViewById(R.id.scrollView);
+        assertNotNull(sv);
+    }
+
+    @Test
+    public void testUBLostTextViewExists(){
+        TextView tv = lT.findViewById(R.id.UBlostTextView);
+        assertNotNull(tv);
+    }
+
+    @Test
+    public void testHelpTextViewExists(){
+        TextView tv = lT.findViewById(R.id.helpTextView);
+        assertNotNull(tv);
+    }
+
+    @Test
+    public void testUsernameTextViewExists(){
+        AutoCompleteTextView atv = lT.findViewById(R.id.username);
+        assertNotNull(atv);
+    }
+
+    @Test
+    public void testPasswordTextViewExists(){
+        AutoCompleteTextView atv = lT.findViewById(R.id.password);
+        assertNotNull(atv);
+    }
+
+    @Test
+    public void testRadioButtonExists(){
+        RadioButton button = lT.findViewById(R.id.radioButton);
+        assertNotNull(button);
+    }
+
+    @Test
+    public void testLoginButtonExists(){
+        Button button = lT.findViewById(R.id.loginButton);
+        assertNotNull(button);
+    }
+
+
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
