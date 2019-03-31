0aac171c6e43d5ca9c5b4c6000f186e277b1e40c
hhuang36 <hhuang36@buffalo.edu>
2019-03-31 19:26:19 -0400

    I added functionality to the floor options after clicking Capen Library
    In CapenFloorOptions.java, I changed the adapter used to add functionality the Floor 1, Floor 2, and Floor 3 buttons in the ListView. I added onItemClickListeners to each of the items in the list. So far, they do not bring the user to a new activity with the floor plan, but we plan to add that functionality soon.

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/myapplication/CapenFloorOptions.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/myapplication/CapenFloorOptions.java
@@ -1,13 +1,18 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,6 +22,8 @@ public class CapenFloorOptions extends AppCompatActivity {
 
     ListView search_capen;
     ArrayAdapter<String> adapter;
+
+    String capenFloorButtons[] = new String [] {"Floor 1", "Floor 2", "Floor 3"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,18 +31,51 @@ public class CapenFloorOptions extends AppCompatActivity {
 
         search_capen = findViewById(R.id.search_capen_floors);
 
-        ArrayList<String> arrayBuildings = new ArrayList<String>();
-        arrayBuildings.addAll(Arrays.asList(getResources().getStringArray(R.array.search_capen_floors)));
-
-        adapter = new ArrayAdapter<String>(
-                CapenFloorOptions.this,
-                android.R.layout.simple_list_item_1,
-                arrayBuildings
-        );
-
+//        ArrayList<String> arrayBuildings = new ArrayList<String>();
+//        arrayBuildings.addAll(Arrays.asList(getResources().getStringArray(R.array.search_capen_floors)));
+//
+//        adapter = new ArrayAdapter<String>(
+//                CapenFloorOptions.this,
+//                android.R.layout.simple_list_item_1,
+//                arrayBuildings
+//        );
+//        search_capen.setAdapter(adapter);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, capenFloorButtons);
         search_capen.setAdapter(adapter);
+        search_capen.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(capenFloorButtons[position] == "Floor 1"){
+                    Toast.makeText(CapenFloorOptions.this, "Testing", Toast.LENGTH_SHORT).show();
+//                    Intent intent = new Intent(CapenFloorOptions.this, capenFloor1Plan.class);
+//                    startActivity(intent);
+                } else if(capenFloorButtons[position] == "Floor 2"){
+//                    Intent intent = new Intent(CapenFloorOptions.this, capenFloor2Plan.class);
+//                    startActivity(intent);
+                } else if(capenFloorButtons[position] == "Floor 3"){
+//                    Intent intent = new Intent(CapenFloorOptions.this, capenFloor3Plan.class);
+//                    startActivity(intent);
+                }
+            }
+        });
+
     }
 
+//    listview.setOnItemClickListener(new OnItemClickListener(){
+//        @Override
+//        public void onItemClick(AdapterView<?>adapter,View v, int position){
+//            ItemClicked item = adapter.getItemAtPosition(position);
+//
+//            Intent intent = new Intent(Activity.this,destinationActivity.class);
+//            //based on item add info to intent
+//            startActivity(intent);
+//        }
+//    });
+//    in your adapter's getItem you write
+//
+//    public ItemClicked getItem(int position){
+//        return items.get(position);
+//    }
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        MenuInflater inflater = getMenuInflater();
