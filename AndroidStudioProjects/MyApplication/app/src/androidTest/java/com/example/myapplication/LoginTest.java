860b40b88e2da42e74f3d2191cb2f3cc7c3c9ffb
hhuang36 <hhuang36@buffalo.edu>
2019-04-11 18:20:35 -0400

    added test to LoginTest.java.
    I added a test that uses espresso to fill in all the login fields with incorrect credentials. Afterwards, I automate AS to click on the login button. The test checks that after this action, the Login Page should still appear on the screen because incorrect credentials are entered.

--- AndroidStudioProjects/MyApplication/app/src/androidTest/java/com/example/myapplication/LoginTest.java
+++ AndroidStudioProjects/MyApplication/app/src/androidTest/java/com/example/myapplication/LoginTest.java
@@ -5,6 +5,7 @@ import android.app.Instrumentation;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -23,6 +24,8 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
@@ -32,6 +35,7 @@ public class LoginTest {
     public ActivityTestRule<Login> loginActivityTestRule = new ActivityTestRule(Login.class);
     public Login lT = null;
     Instrumentation.ActivityMonitor homePage = getInstrumentation().addMonitor(SplashScreen.class.getName(), null, false);
+    Instrumentation.ActivityMonitor loginPage = getInstrumentation().addMonitor(Login.class.getName(), null, false);
 
     @Before
     public void setUp() throws Exception {
@@ -87,20 +91,33 @@ public class LoginTest {
         assertNotNull(button);
     }
 
+    // tests that when correct credentials are entered that the user is brought to the home page
     @Test
     public void testLoginButtonFunctionality(){
         ViewInteraction appCompatAutoCompleteTextView = onView(withId(R.id.email));
         appCompatAutoCompleteTextView.perform(replaceText("hartloff@gmail.com"), closeSoftKeyboard());
 
-        // click email & type in test@gmail.com
         ViewInteraction appCompatAutoCompleteTextView2 = onView(withId(R.id.password));
         appCompatAutoCompleteTextView2.perform(replaceText("hartloff"), closeSoftKeyboard());
 
-        // click on register button
         onView(withId(R.id.loginButton)).perform(click());
         Activity home = getInstrumentation().waitForMonitorWithTimeout(homePage, 5000);
         assertNotNull(home);
         home.finish();
+    }
+
+    @Test
+    public void testLoginButtonIncorrectCredentialsFunctionality(){
+        ViewInteraction appCompatAutoCompleteTextView = onView(withId(R.id.email));
+        appCompatAutoCompleteTextView.perform(replaceText("hartloff@gmail.com"), closeSoftKeyboard());
+
+        ViewInteraction appCompatAutoCompleteTextView2 = onView(withId(R.id.password));
+        appCompatAutoCompleteTextView2.perform(replaceText("hartlof"), closeSoftKeyboard());
+
+        onView(withId(R.id.loginButton)).perform(click());
+        Activity login = getInstrumentation().waitForMonitorWithTimeout(loginPage, 5000);
+        assertNotNull(login);
+        login.finish();
     }
 
     @After
