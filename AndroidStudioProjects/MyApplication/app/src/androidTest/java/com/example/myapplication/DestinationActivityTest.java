82e72a30969f50ac4e4d8be61662177536f3827a
hhuang36 <hhuang36@buffalo.edu>
2019-04-14 17:09:27 -0400

    Added test to DestinationActivityTest.java
    I created the activity test rule for the destination page. I also created the first test, which tests that the first option displayed on the list of searchable options displays 'Capen Library'. I will add tests for the other options as well.

--- AndroidStudioProjects/MyApplication/app/src/androidTest/java/com/example/myapplication/DestinationActivityTest.java
+++ AndroidStudioProjects/MyApplication/app/src/androidTest/java/com/example/myapplication/DestinationActivityTest.java
@@ -1,17 +1,36 @@
 package com.example.myapplication;
 
+import android.support.test.rule.ActivityTestRule;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DestinationActivityTest {
 
+    @Rule
+    public ActivityTestRule<DestinationActivity> destinationActivityTestRule = new ActivityTestRule(DestinationActivity.class);
+
+    DestinationActivity destination = null;
     @Before
     public void setUp() throws Exception {
+        destination = destinationActivityTestRule.getActivity();
+    }
+
+    // test that the first option shows the text "Capen Library"
+    @Test
+    public void testBuildingOptionsStringArray1(){
+        String BuildingOptions[] = new String [] {"Capen Library", "Lockwood Library", "Norton Hall", "Davis Hall", "Music Library"};
+        assertEquals(BuildingOptions[0], destination.adapter.getItem(0));
     }
 
     @After
     public void tearDown() throws Exception {
+        destination = null;
     }
+
+
 }
