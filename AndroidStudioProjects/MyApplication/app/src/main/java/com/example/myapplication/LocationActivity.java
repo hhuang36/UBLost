9a712078acbb34fe1f399a33199511f150096fe9
hhuang36 <hhuang36@buffalo.edu>
2019-04-17 11:39:49 -0400

    completed layout of LocationActivity.java
    I implemented the button listeners for all the options in the list view. I have also implemented a clickable textview, that allows the user to get their current location based on Wifi. Additionally, I have also added the hint for the search bar and changed the text colors/layout in both LocationActivity.java and DestinationActivity.java.

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/myapplication/LocationActivity.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/myapplication/LocationActivity.java
@@ -1,8 +1,18 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,10 +28,10 @@ public class LocationActivity extends AppCompatActivity {
 
     ListView search_location;
     ArrayAdapter<String> adapter;
-    TextView LocationTextView;
-    String buildingButtons[] = new String [] {"Capen Library", "Lockwood Library", "Norton Hall", "Davis Hall", "Music Library", "Natural Science Complex", "Student Union", "Knox Hall"};
+    TextView locationTextView;
+    String locationButtons[] = new String [] {"Capen Library", "Lockwood Library", "Norton Hall", "Davis Hall", "Music Library", "Natural Science Complex", "Student Union", "Knox Hall"};
     SearchView searchView;
-    Button getCurrentLocation;
+    TextView getCurrentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +44,7 @@ public class LocationActivity extends AppCompatActivity {
         arrayLocations.addAll(Arrays.asList(getResources().getStringArray(R.array.my_locations)));
 
         adapter = new ArrayAdapter<String>(
-                LocationActivity.this,
+                getApplicationContext(),
                 android.R.layout.simple_list_item_1,
                 arrayLocations
         );
@@ -44,33 +54,76 @@ public class LocationActivity extends AppCompatActivity {
         search_location.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if(buildingButtons[position] == "Capen Library"){
+                if(locationButtons[position] == "Capen Library"){
                     Intent intent = new Intent(LocationActivity.this, CapenFloor1Plan.class);
                     startActivity(intent);
-                } else if(buildingButtons[position] == "Lockwood Library"){
+                } else if(locationButtons[position] == "Lockwood Library"){
                     Intent intent = new Intent(LocationActivity.this, CapenFloor1Plan.class);
                     startActivity(intent);
-                } else if(buildingButtons[position] == "Norton Hall"){
+                } else if(locationButtons[position] == "Norton Hall"){
                     Intent intent = new Intent(LocationActivity.this, CapenFloor1Plan.class);
                     startActivity(intent);
-                } else if(buildingButtons[position] == "Davis Hall"){
+                } else if(locationButtons[position] == "Davis Hall"){
                     Intent intent = new Intent(LocationActivity.this, CapenFloor1Plan.class);
                     startActivity(intent);
-                } else if(buildingButtons[position] == "Music Library"){
+                } else if(locationButtons[position] == "Music Library"){
                     Intent intent = new Intent(LocationActivity.this, CapenFloor1Plan.class);
                     startActivity(intent);
-                } else if(buildingButtons[position] == "Natural Science Complex"){
+                } else if(locationButtons[position] == "Natural Science Complex"){
                     Intent intent = new Intent(LocationActivity.this, CapenFloor1Plan.class);
                     startActivity(intent);
-                } else if(buildingButtons[position] == "Student Union"){
+                } else if(locationButtons[position] == "Student Union"){
                     Intent intent = new Intent(LocationActivity.this, CapenFloor1Plan.class);
                     startActivity(intent);
-                } else if(buildingButtons[position] == "Knox Hall"){
+                } else if(locationButtons[position] == "Knox Hall"){
                     Intent intent = new Intent(LocationActivity.this, CapenFloor1Plan.class);
                     startActivity(intent);
                 }
             }
         });
 
+        locationTextView = findViewById(R.id.locationTextView);
+        getCurrentLocation = findViewById(R.id.locateMeButton);
+
+        String text = "Find my location!";
+        SpannableString ss = new SpannableString(text);
+        ForegroundColorSpan color = new ForegroundColorSpan(getResources().getColor(R.color.colorPrimaryDark));
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Intent intent = new Intent(LocationActivity.this, DestinationActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        ss.setSpan(clickableSpan, 0, 16, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(color, 0, 16, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        getCurrentLocation.setText(ss);
+        getCurrentLocation.setMovementMethod(LinkMovementMethod.getInstance());
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_location_menu, menu);
+        MenuItem item = menu.findItem(R.id.search_location);
+        searchView = (SearchView) item.getActionView();
+        searchView.setQueryHint("Search Location...");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
