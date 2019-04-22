847f6c6055fbb363269f34b4b60666c799493b8d
Diana Gao <dee.gao12@gmail.com>
2019-04-22 19:01:07 -0400

    added additional image files
    The image files are used in the xml layout files for different activities. My initial push did not add them

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/myapplication/HomeScreen.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/myapplication/HomeScreen.java
@@ -1,16 +1,23 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 public class HomeScreen extends AppCompatActivity { // THIS IS THE HOME PAGE
     private DrawerLayout menuDrawerLayout;
@@ -18,6 +25,8 @@ public class HomeScreen extends AppCompatActivity { // THIS IS THE HOME PAGE
      Button floorplanButton;
      Button navigateMe;
     private Button Home;
+    private static final String TAG = "HomeScreen";
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
 
     @Override
@@ -26,6 +35,15 @@ public class HomeScreen extends AppCompatActivity { // THIS IS THE HOME PAGE
         setContentView(R.layout.activity_main);
 
         Home = findViewById(R.id.home_page);
+
+//        // Construct a GeoDataClient.
+//        mGeoDataClient = Places.getGeoDataClient(this, null);
+//
+//        // Construct a PlaceDetectionClient.
+//        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+//
+//        // Construct a FusedLocationProviderClient.
+//        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
 
         menuDrawerLayout = findViewById(R.id.drawerLayout);
@@ -88,6 +106,28 @@ public class HomeScreen extends AppCompatActivity { // THIS IS THE HOME PAGE
         }
         return super.onOptionsItemSelected(x);
     }
+
+//    private void getDeviceLocation() {
+//        Log.d(TAG, "getDeviceLocation: getting the device current location");
+//        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+//    }
+//
+//    private void getLocationPermission() {
+//        /*
+//         * Request location permission, so that we can get the location of the
+//         * device. The result of the permission request is handled by a callback,
+//         * onRequestPermissionsResult.
+//         */
+//        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+//                android.Manifest.permission.ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            mLocationPermissionGranted = true;
+//        } else {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+//                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+//        }
+//    }
 
 }
 
