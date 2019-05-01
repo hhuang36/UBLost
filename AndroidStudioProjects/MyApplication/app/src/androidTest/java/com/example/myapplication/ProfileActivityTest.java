e6961a48c638504b61351ae518bdc1359c6e2a33
hhuang36 <hhuang36@buffalo.edu>
2019-04-30 22:22:35 -0400

    Added comments for all tests in ProfileActivity.java
    The heading is pretty straightfoward- for each test I wrote, I made sure to write above the test declaration its purpose and functionality.

--- AndroidStudioProjects/MyApplication/app/src/androidTest/java/com/example/myapplication/ProfileActivityTest.java
+++ AndroidStudioProjects/MyApplication/app/src/androidTest/java/com/example/myapplication/ProfileActivityTest.java
@@ -29,22 +29,26 @@ public class ProfileActivityTest {
     private Instrumentation.ActivityMonitor monitor1 = getInstrumentation().addMonitor(PubPathsActivity.class.getName(), null, false);
     private Instrumentation.ActivityMonitor monitor2 = getInstrumentation().addMonitor(SettingsActivity.class.getName(), null, false);
 
+    // initializing the activity
     @Before
     public void setUp() throws Exception {
         profileActivity = profileActivityActivityTestRule.getActivity();
     }
 
+    // tests that the activity is not null when the application is run
     @Test
     public void testActivityNotNull(){
         assertNotNull(profileActivity);
     }
 
+    // tests that the image view for the user profile exists
     @Test
     public void testProfileAvatarImageViewExists(){
         ImageView avatar = profileActivity.profileAvatar;
         assertNotNull(avatar);
     }
 
+    // tests that the correct image view is used for the profile avatar
     @Test
     public void testProfileAvatarImageViewHasCorrectID(){
         ImageView avatar = profileActivity.profileAvatar;
@@ -53,12 +57,14 @@ public class ProfileActivityTest {
         assertEquals(ID, expectedID);
     }
 
+    // tests that the text view for the profile name exists
     @Test
     public void testProfileNameTextViewExists(){
         TextView profileName = profileActivity.profileName;
         assertNotNull(profileName);
     }
 
+    // tests that the correct text view is used for the profile name
     @Test
     public void testProfileNameTextViewHasCorrectID(){
         TextView profileName = profileActivity.profileName;
@@ -67,13 +73,14 @@ public class ProfileActivityTest {
         assertEquals(ID, expectedID);
     }
 
-
+    // tests that the text view for the profile status exists
     @Test
     public void testProfileStatusTextViewExists(){
         TextView profileStatus = profileActivity.profileStatus;
         assertNotNull(profileStatus);
     }
 
+    // tests that the correct text view is used for the profile status
     @Test
     public void testProfileStatusTextViewHasCorrectID(){
         TextView profileStatus = profileActivity.profileStatus;
@@ -82,12 +89,14 @@ public class ProfileActivityTest {
         assertEquals(ID, expectedID);
     }
 
+    // tests that the list view for the profile options exists
     @Test
     public void testListViewExists(){
         ListView profileOptions = profileActivity.profile_options;
         assertNotNull(profileOptions);
     }
 
+    // tests that the correct list view is used for the profile options
     @Test
     public void testListViewHasCorrectID(){
         ListView profileOptions = profileActivity.profile_options;
@@ -96,30 +105,35 @@ public class ProfileActivityTest {
         assertEquals(ID, expectedID);
     }
 
+    // tests that the adapter for the profile options list view exists
     @Test
     public void testAdapterExists(){
         Adapter adapter = profileActivity.adapter;
         assertNotNull(adapter);
     }
 
+    // tests that the first option in the list view displays exactly "Messages"
     @Test
     public void testProfileOptionsArray1(){
         String[] profileOptions = new String[]{"Messages", "My Paths", "My Account"};
         assertEquals(profileOptions[0], profileActivity.profileOptions[0]);
     }
 
+    // tests that the second option in the list view displays exactly "My Paths"
     @Test
     public void testProfileOptionsArray2(){
         String[] profileOptions = new String[]{"Messages", "My Paths", "My Account"};
         assertEquals(profileOptions[1], profileActivity.profileOptions[1]);
     }
 
+    // tests that the third option in the list view displays exactly "My Account"
     @Test
     public void testProfileOptionsArray3(){
         String[] profileOptions = new String[]{"Messages", "My Paths", "My Account"};
         assertEquals(profileOptions[2], profileActivity.profileOptions[2]);
     }
 
+    // tests that the first option in the list view when clicked brings user to MessagesActivity.java
     @Test
     public void testMessagesOption(){
         final ListView lv = profileActivity.profile_options;
@@ -136,6 +150,7 @@ public class ProfileActivityTest {
         secondActivity.finish();
     }
 
+    // tests that the second option in the list view when clicked brings user to PubPathsActivity.java
     @Test
     public void testMyPathsOption(){
         final ListView lv = profileActivity.profile_options;
@@ -152,6 +167,7 @@ public class ProfileActivityTest {
         secondActivity.finish();
     }
 
+    // tests that the third option in the list view when clicked brings user to SettingsActivity.java
     @Test
     public void testMyAccountOption(){
         final ListView lv = profileActivity.profile_options;
@@ -168,12 +184,14 @@ public class ProfileActivityTest {
         secondActivity.finish();
     }
 
+    // tests that the bottom navigation bar for the profile activity exists
     @Test
     public void testBottomMenuBarExists(){
         BottomNavigationView bottomNavigationView = profileActivity.bottomNavigationView;
         assertNotNull(bottomNavigationView);
     }
 
+    // tests that the bottom navigation bar for the profile activity uses the correct menu xml file
     @Test
     public void testBottomMenuBarHasCorrectID(){
         BottomNavigationView bottomNavigationView = profileActivity.bottomNavigationView;
@@ -182,6 +200,7 @@ public class ProfileActivityTest {
         assertEquals(ID, expectedID);
     }
 
+    // setting profileActivity to null to end testing
     @After
     public void tearDown() throws Exception {
         profileActivity = null;
