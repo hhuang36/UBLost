a12a4e7f4c31b6389cd2000a0818361627b843b7
maygnificent <okaybreakfast@gmail.com>
2019-05-09 21:17:10 -0400

    Created FriendsActivity.java and activity_friends.xml
    Added textview 'labelFriends' and  RecyclerView 'listFriends' to activity_friends.xml
    Added 'friends' list parameter to Model:User.java
    
    To Do:
    * Search bar in activity_friends.xml
    * Add Friends button to activity_profile.xml
    * Connect activity_profile.xml to activity_friends.xml

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/myapplication/Model/User.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/myapplication/Model/User.java
@@ -1,17 +1,27 @@
 package com.example.myapplication.Model;
 
+import java.util.List;
+
 public class User {
 
     private String password;
     private String email;
     private String status;
+    private List friends;
 
-    public User(String password, String email, String status){
+    public User(String password, String email, String status, List friends){
         this.password = password;
         this.email = email;
         this.status = status;
+        this.friends = friends;
     }
 
+    public List getFriends() {
+        return friends;
+    }
+    public void addFriend(String user) {
+        friends.add(user);
+    }
     public String getPassword() {
         return password;
     }
