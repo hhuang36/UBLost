d078ebe6a71358d7f3402dec614d2e55f62e7f5e
maygnificent <okaybreakfast@gmail.com>
2019-05-10 05:24:14 -0400

    Added functionality to 'FriendsActivity.java'
    Created 'Friends' parent node in UBLost FirebaseDatabase
    Added 'ValueEventListener' method to 'friends' database method to read and write new friends information to the 'listfriends' listview
    
    User Story 6 functionality is now complete.

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/myapplication/FriendsActivity.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/myapplication/FriendsActivity.java
@@ -1,21 +1,39 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.myapplication.Model.Friends;
+import com.example.myapplication.Model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class FriendsActivity extends AppCompatActivity {
 
     //database
     private FirebaseDatabase database;
     private DatabaseReference friends;
-
+    private DatabaseReference users;
+    private FirebaseAuth mAuth;
 
     ListView listFriends;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> list_Friends = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +44,47 @@ public class FriendsActivity extends AppCompatActivity {
         //database
         database = FirebaseDatabase.getInstance();
         friends = database.getReference("Friends");
+        users = database.getReference("Users");
+        mAuth = FirebaseAuth.getInstance();
+
+        final String currentUser = mAuth.getCurrentUser().getUid();
+
+        //show friends
+        friends.child(currentUser).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot dbListFriends: dataSnapshot.getChildren()){
+                    //get UID of riends
+                    String friendUID = dbListFriends.getKey().toString();
+                    //search for UID in Users
+                    users.child(friendUID).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            for (DataSnapshot credentials: dataSnapshot.getChildren()){
+                                if(credentials.getKey().equalsIgnoreCase("email")) {
+                                    String myFriend = credentials.getValue().toString();
+                                    myFriend = myFriend.replace("@gmail.com", "");
+                                    //add friends to list
+                                    list_Friends.add(myFriend);
+                                    adapter = new ArrayAdapter<>(FriendsActivity.this, android.R.layout.simple_list_item_1, list_Friends);
+                                    listFriends.setAdapter(adapter);
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
